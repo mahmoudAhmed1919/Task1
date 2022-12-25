a205815dbd7c8d853f8447e10259e92b6900af83
mahmoudAhmed1919 <121295627+mahmoudAhmed1919@users.noreply.github.com>
2022-12-25 23:43:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Mahmoud Ahmed Elsherbiny Ahmed ... Section 21 ... Discrete Mathematics Project 1
+            Console.WriteLine("Enter two numbers to get the prime numbers between them : ");
+            int n1, n2;
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+
+            for (int i = n1; i <= n2; ++i) {
+                if (i == 1) continue;
+                bool status = true;
+                for (int j = i / 2; j >= 2; j--) {
+                    if (i % j == 0)
+                    {
+                        status = false;
+                        break;
+                    }
+                }
+                if (status) Console.WriteLine(i);
+            }
+        }
+    }
+}
